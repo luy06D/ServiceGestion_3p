93a39b00fc9fb37aedfb540b06b2c14e552eb2e5
Luy06 <cusiluis04@gmail.com>
2023-09-09 16:59:50 -0500

    Template sistema 3p

---
+++ views/index.php
@@ -0,0 +1,20 @@
+
+  
+  <?php include '../principal/cabezera.php' ?>
+
+
+
+  <main id="main" class="main">
+
+  <div class="container">
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis, saepe dolores. Repellendus ipsa laboriosam optio necessitatibus cum dolorum tenetur, sint sed dicta assumenda doloremque? Aperiam magnam quibusdam nesciunt temporibus ad?<
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis, saepe dolores. Repellendus ipsa laboriosam optio necessitatibus cum dolorum tenetur, sint sed dicta assumenda doloremque? Aperiam magnam quibusdam nesciunt temporibus ad?<
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis, saepe dolores. Repellendus ipsa laboriosam optio necessitatibus cum dolorum tenetur, sint sed dicta assumenda doloremque? Aperiam magnam quibusdam nesciunt temporibus ad?<
+    
+  </div>
+
+  </main>
+ 
+
+  <?php include '../principal/footer.php' ?>
+
